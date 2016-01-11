d17a5f42039ea3668654a014e75bbeee0e88f642
Scott <item0932@gmail.com>
2016-01-11 16:36:01 +0800

    減少票的功能“ dfdfdf
    exit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111053448) do
+ActiveRecord::Schema.define(version: 20160111082731) do
+
+  create_table "members", force: :cascade do |t|
+    t.string   "name"
+    t.string   "pwd"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
